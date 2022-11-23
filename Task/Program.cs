0de1639633bb6e94a015fbafa19237eaa5ff80d7
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-11-23 18:04:58 +0300

    Initial commit.Add task and gitignore

---
+++ Task/Program.cs
@@ -0,0 +1,32 @@
+﻿void Main()
+{   
+    Console.Clear();
+    string[] first_array = new string[4] {"hello", "2", "world", ":-)"};
+    string[] second_array = new string[first_array.Length];
+    CreateResultArrayWithIf(first_array, second_array);
+    PrintArray(second_array);
+}
+
+void CreateResultArrayWithIf(string[] first_array, string[] second_array)
+{
+    int count = 0;
+    for (int i = 0; i < first_array.Length; i++)
+    {
+    if(first_array[i].Length <= 3)
+        {
+        second_array[count] = first_array[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Main();
